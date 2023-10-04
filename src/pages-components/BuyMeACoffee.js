7d00c169f42dde7ab1bf53f7b0147049f8b2bbe5
Nilushanth Thiruchelvam <Nilu@Nilus-iMac.local>
2023-10-04 04:34:42 -0400

    Added styling

--- src/pages-components/BuyMeACoffee.js
+++ src/pages-components/BuyMeACoffee.js
@@ -6,7 +6,7 @@ const BuyMeACoffee = () => {
             <p>
               Hi, I'm Nilushanth. I created this platform, PathMind, as a sandbox to hone my web development skills. My hope is that it can help others maintain an organized and goal-oriented learning journey. The source code for this site is open-source and can be found here: 
               <a href="https://github.com/nilu-t">GitHub Repository</a>. 
-              If you find this platform useful, please consider supporting my work through the following BuyMeACoffee link: 
+              If you find this platform useful, consider supporting my work through the following BuyMeACoffee link: 
               <Link to="/BuyMeACoffee">Support with a Coffee ☕</Link>
             </p>
         </div>

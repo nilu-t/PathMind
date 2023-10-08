4de083e2b182ccf6bfe71f3de1781205375f30a4
Nilushanth Thiruchelvam <Nilu@Nilus-iMac.local>
2023-10-07 20:23:24 -0400

    dynamically added images for cs path

--- src/pages-components/MyPath.js
+++ src/pages-components/MyPath.js
@@ -1,22 +1,46 @@
-import csLearningPaths from './csLearningPaths.json'
+import csLearningPaths from './csLearningPaths.json';
+import React, { useState } from 'react';
 
 const MyPath = () => {
 
-    // const learningPathToFind = "Cybersecurity";
-    // const path = csLearningPaths.learningPaths.find( (lPath) => lPath.title === learningPathToFind);
-
-
     const myLearningPaths = csLearningPaths.learningPaths;
+
+    const convertToImageNamePNG = (subject) =>{
+        return `${subject.toLowerCase().replaceAll("+","plus").replace("#","-sharp").replace(".","").replaceAll(" ", "-")}-logo.png`
+    }
+
+    const handleFileUpload = (title, event) => {
+        const file = event.target.files[0];
+        console.log(`File uploaded for ${title}: ${file.name}`);
+        //handling the uploaded file. i.e, extract the text and upload the note to the database. 
+
+    };
 
     return(
         <div id="my-path-div">
-            { myLearningPaths.map( (pathObj) => 
-                <div className="path-card">
-                    <h1> { pathObj.title } </h1>
+            { myLearningPaths.map( (pathObj, index) => {
+                const subjectsArr = pathObj.subjects;
+
+                const images = subjectsArr.map((subject) =>{
+                    const imagePath = `learning-path-images/computer-science/${convertToImageNamePNG(subject)}`; //from the public folder so the path can be accessed from there. 
+
+                    return <img src={imagePath} alt={imagePath}/>
+                })
+
+                return(
+                <div className="path-card" key={index}>
+                    <h1 id="path-title"> { pathObj.title } </h1>
+                    <p id="path-desc">{ pathObj.description }</p>
+
+                    <div id="path-images">
+                        { images }
+                    </div>
                 </div>
-            ) }
+                );
+
+            })}
         </div>
-    )
+    );
 }
 
 export default MyPath;
